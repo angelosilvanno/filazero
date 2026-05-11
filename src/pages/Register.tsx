f02f87a637c2570add4e5e000d6712be3c008f13
Ângelo <familiasabino14@gmail.com>
2026-05-11 15:41:39 -0300

    feat: atualizar texto do placeholder para campos de CPF e telefone

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -54,7 +54,7 @@ export const Register = () => {
           
           <input 
             type="text" 
-            placeholder="CPF (000.000.000-00)" 
+            placeholder="CPF" 
             value={cpf}
             onChange={(e) => setCpf(handleCpf(e.target.value))}
             className="w-full p-4 bg-slate-50 border-2 border-slate-100 rounded-2xl focus:border-blue-500 outline-none transition-all" 
@@ -64,7 +64,7 @@ export const Register = () => {
           
           <input 
             type="text" 
-            placeholder="Telefone ((00) 00000-0000)" 
+            placeholder="Telefone" 
             value={phone}
             onChange={(e) => setPhone(handlePhone(e.target.value))}
             className="w-full p-4 bg-slate-50 border-2 border-slate-100 rounded-2xl focus:border-blue-500 outline-none transition-all" 
